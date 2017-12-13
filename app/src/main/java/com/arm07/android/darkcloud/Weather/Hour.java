ae9129c1920b5d186eafef607fbb025ca287316f
arm07 <rallam2@uic.edu>
2017-12-13 01:03:48 -0600

    Hourly Forecast Activity attached to adapter

--- app/src/main/java/com/arm07/android/darkcloud/Weather/Hour.java
+++ app/src/main/java/com/arm07/android/darkcloud/Weather/Hour.java
@@ -1,15 +1,23 @@
 package com.arm07.android.darkcloud.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  * Created by rashmi on 11/4/2017.
  */
 
-public class Hour {
+public class Hour implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimeZone;
+
+    public Hour() {}
 
     public long getTime() {
         return mTime;
@@ -50,4 +58,48 @@ public class Hour {
     public void setTimeZone(String timeZone) {
         mTimeZone = timeZone;
     }
+
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
+    }
+
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        Date date = new Date(mTime * 1000);
+        return formatter.format(date);
+    }
+
+    @Override
+    public int describeContents() {
+        return 0; // ignore
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeDouble(mTemperature);
+        dest.writeString(mSummary);
+        dest.writeString(mIcon);
+        dest.writeString(mTimeZone);
+    }
+
+    private Hour(Parcel in) {
+        mTime = in.readLong();
+        mTemperature = in.readDouble();
+        mSummary = in.readString();
+        mIcon = in.readString();
+        mTimeZone = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            return new Hour(source);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 }
